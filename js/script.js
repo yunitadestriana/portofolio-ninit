a2cd757f969e009f56636878a5c322b5a8d74703
Yunita Destriana <yunitadestriana@gmail.com>
2020-05-22 14:45:05 +0700

    menambahkan file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//	event pada saat link di klik
+$('.page-scroll').on('click', function () {
+
+	//	ambil isi href
+	var tujuan = $(this).attr('href');
+	//	tangkap elemen ybs
+	var	elemenTujuan = $(tujuan);
+
+	// pindahkan scroll
+	$('html,body').animate({
+		scrollTop: elemenTujuan.offset().top-50
+	}, 1000, 'swing');
+
+	e.preventDefault();
+});
